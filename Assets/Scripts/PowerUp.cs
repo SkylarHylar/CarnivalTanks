051f0580a9725ec629ee9ea4c06e5770df0fcb30
SkylarHylar <skylariobst65@gmail.com>
2019-09-13 13:54:27 -0400

    Added a WIP Powerup Box with a texture, and created a simple Airstrike/Nuke object.

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    Rigidbody rigid;
     void Start()
     {
-        
+        rigid = gameObject.GetComponent<Rigidbody>();
     }
     void Update()
     {
-
+        
     }
 
 }
